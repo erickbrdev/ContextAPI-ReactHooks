4e6d7791da112a21873dd83b1a83862ec610654b
Erick-Andrade-Dev <erick.dev07@gmail.com>
2022-10-31 10:54:45 -0300

    useState

---
+++ react-hooks/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(0)
+  return (
+    <div>
+      <h3>Contador: { counter }</h3>
+      <button 
+        type="button"
+        onClick={() => setCounter(counter +1 )}  
+      >
+        Adicionar
+      </button>
+      <button
+        type="button"
+        onClick={() => setCounter(counter -1)}  
+      >
+        Diminuir
+      </button>
+    </div>
+  );
+}
+
+export default App;
